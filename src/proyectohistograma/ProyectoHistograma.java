2d3c14ed1e6670528730b9f318666e520c73ba46
Enrique José Ramón Balmaseda <ejrbalma@gmail.com>
2017-10-14 13:33:05 +0100

    ADD Primera version

---
+++ src/proyectohistograma/ProyectoHistograma.java
@@ -0,0 +1,20 @@
+package proyectohistograma;
+
+import java.util.HashMap;
+
+public class ProyectoHistograma {
+
+    public static void main(String[] args) {
+        
+        int[] vector = {1, 3, 5, 7, 9, 1, 5, 4, 3, 9,100,-4};
+        
+        Histogram mihisto = new Histogram(vector);
+        HashMap<Integer, Integer> histogram = mihisto.getHisto();
+        
+        for (Integer key : histogram.keySet()) {
+            System.out.println(key + " --> " + histogram.get(key));
+        }
+        
+    }
+    
+}
