b2dd9f117acecf59d5ccee2648a105e08380668e
Enrique José Ramón Balmaseda <ejrbalma@gmail.com>
2017-10-14 14:57:14 +0100

    UPDATE Tercera version MCV

--- src/proyectohistograma/ProyectoHistograma.java
+++ src/proyectohistograma/ProyectoHistograma.java
@@ -6,12 +6,13 @@ public class ProyectoHistograma {
 
     public static void main(String[] args) {
         
-        //Integer [] vector = {1, 3, 5, 7, 9, 1, 5, 4, 3, 9,100,-4};
-        String[] vector = {"Ana", "Juan", "Elena", "Elena", "Juan", "Ana", "Pedro"};
-        Histogram mihisto = new Histogram(vector);
-        HashMap<Object, Integer> histogram = mihisto.getHisto();
+        Integer [] vector = {1, 3, 5, 7, 9, 1, 5, 4, 3, 9,100,-4};
+        // String[] vector = {"Ana", "Juan", "Elena", "Elena", "Juan", "Ana", "Pedro"};
+
         
-        for (Object key : histogram.keySet()) {
+        Histogram <Integer> histogram = CalculaHistogram.computeHistogram(vector);
+        
+        for (Integer key : histogram.keySet()) {
             System.out.println(key + " --> " + histogram.get(key));
         }
         
